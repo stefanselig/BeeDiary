c19a61f8f0180fe0b251ed4e07e312383801cae0
stefanselig <stefan.selig1@gmail.com>
2015-12-28 18:42:37 +0100

    Implemented basic rest api

--- beekeeper-client/app/boot.ts
+++ beekeeper-client/app/boot.ts
@@ -1,9 +1,11 @@
 import {AppComponent} from './main/app.component';
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
+import {HTTP_BINDINGS} from 'angular2/http';
 
 class RouterComponent {}
 
 bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
+  HTTP_BINDINGS
 ]);
