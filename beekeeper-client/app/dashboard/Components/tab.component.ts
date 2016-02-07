8dfe678bfd15a0c08febf49328980af356dce103
stefan2 <stefan.selig1@gmail.com>
2016-02-07 22:56:11 +0100

    Added charts for visualization.

---
+++ beekeeper-client/app/dashboard/Components/tab.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+import {DashBoardService}	from '../services/dashboard.service';
+
+@Component({
+	selector: 'Tab',
+	template: ``,
+	providers: [DashBoardService]
+})
+export class Tab {
+}
