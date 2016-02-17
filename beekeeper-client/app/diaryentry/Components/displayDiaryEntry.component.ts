96ff7e6336f45b5c2c8bf8542455bb47648f97e1
stefan2 <stefan.selig1@gmail.com>
2016-02-17 23:16:28 +0100

    Added required fields + added delete functionality + added date formatting + some minor improvements and bugfixes.

--- beekeeper-client/app/diaryentry/Components/displayDiaryEntry.component.ts
+++ beekeeper-client/app/diaryentry/Components/displayDiaryEntry.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, AfterViewInit}	from 'angular2/core';
-import {Router, RouteParams}				from 'angular2/router';
+import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {Router, RouteParams} from 'angular2/router';
 
-import {DiaryEntryService}	from '../services/diaryentry.service';
+import {DiaryEntryService} from '../services/diaryentry.service';
 
 import * as DiaryEntryModule from './../../build-client/DiaryEntry/DiaryEntry';
 
@@ -9,16 +9,19 @@ import * as DiaryEntryModule from './../../build-client/DiaryEntry/DiaryEntry';
 	selector: 'displaydiaryentry',
 	templateUrl: 'app/diaryentry/Templates/displayDiaryEntry.template.html',
 	inputs: ['diaryentry'],
-	providers: [DiaryEntryService]
+	outputs: ['onDiaryEntryDeleted']
 })
-export class DisplayDiaryEntryComponent implements OnInit, AfterViewInit {
+export class DisplayDiaryEntryComponent  {
 	public diaryentry: DiaryEntryModule.DiaryEntry = new DiaryEntryModule.DiaryEntry();
+	public onDiaryEntryDeleted: EventEmitter<string> = new EventEmitter<string>();
+	
+	public viewDetails: boolean = false;
 	
 	constructor(public diaryEntryService: DiaryEntryService, public router: Router) {}
 	
-	ngAfterViewInit(): void {}
-	
-	ngOnInit(): void {}
+	public showDetails(): void {
+		this.viewDetails = !this.viewDetails;
+	}
 	
 	public parseMd(): string {
 		if (this.diaryentry.description == null) 
@@ -29,5 +32,23 @@ export class DisplayDiaryEntryComponent implements OnInit, AfterViewInit {
 	
 	public editDiaryEntry(id: number): void {
 		this.router.navigate(['EditDiaryEntry', { id: id }]);
+	}
+	
+	public removeDiaryEntry(id: string): void {
+		this.onDiaryEntryDeleted.emit(id);
+	}
+	
+	public formatDate(date: Date, options): string {
+		if (date == null || date == undefined)
+			return "";
+		const months = ["Jänner", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+		let month;
+		if (options == "fullmonths") {
+			month = months[date.getMonth()];
+		}
+		else {
+			month = date.getMonth()+1;
+		}
+		return `${date.getDate()}. ${month}. ${date.getFullYear()}`;
 	}
 }
