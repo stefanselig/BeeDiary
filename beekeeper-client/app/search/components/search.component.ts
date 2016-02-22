e8df4ca22d1194b3acb923951e01975fc8fbd0b0
stefan2 <stefan.selig1@gmail.com>
2016-02-22 13:27:21 +0100

    Improved search functionality.

---
+++ beekeeper-client/app/search/components/search.component.ts
@@ -0,0 +1,21 @@
+import {Component, EventEmitter} from 'angular2/core';
+
+@Component({
+	selector: 'search',
+	template: `
+		<form (ngSubmit)="onSearch.emit(query)" #searchForm="ngForm" style="display: inline;">
+			<input type="search" (keyup)="onSearch.emit(query)" [(ngModel)]="query" 
+				[size]="('Durchsuche ' + name).length" 
+				[placeholder]="'Durchsuche ' + name" class="form-control"/>
+			<button type="submit" class="btn btn-default form-control">
+				<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+			</button>
+		</form>
+	`,
+	inputs: ['name'],
+	outputs: ['onSearch']
+})
+export class SearchComponent {
+	public name: string = "";
+	public onSearch: EventEmitter<string> = new EventEmitter<string>();
+}
