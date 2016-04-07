01d89ebd9eba924ab8de61fb6d3b1a46311f2a71
stefan2 <stefan.selig1@gmail.com>
2016-04-07 14:22:29 +0200

    Added Typings to github project.

---
+++ beediary-client/typings/tsd.d.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="googlemaps/google.maps.d.ts" />
+/// <reference path="marked/marked.d.ts" />
+/// <reference path="gapi/gapi.auth2/gapi.auth2.d.ts" />
+/// <reference path="gapi/gapi/gapi.d.ts" />
+/// <reference path="google.visualization/google.visualization.d.ts" />
+/// <reference path="../config.d.ts" />
